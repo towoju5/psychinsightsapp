4b481c10c2197b332a6a4deabd7064d03c2ad3bf
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-11-17 09:05:44 +0100

    about page updated

--- about.php
+++ about.php
@@ -11,23 +11,27 @@
 
 <div class="lg:mx-28 xl:mx-56 mt-12">
 	<div class="about__inner">
-		<section class="ceo">
+		<section class="ceo ">
 			<!-- <p class="text-center lg:hidden text-2xl py-8">About Us</p> -->
-			<div class="grid lg:grid-cols-2 gap-20 items-center py-20">
-				<div class="ceo__col ceo__col--1 py-18">
+			<div class="grid lg:grid-cols-2 gap-20 items-center py-20 border-b md:border-b-0">
+				<div class=" py-18">
 					<div class="ceo__text-group">
-						<h3 class="text-center lg:text-left text-[32px] font-bold">Optimize your Mental Health and Live a Better Life</h3>
+						<h3 class="text-center lg:text-left text-[26px] lg:text-[32px] font-bold">Optimize your Mental Health and Live a Better Life</h3>
 						<div class="tick__border-top mx-auto"></div>
 						<small class="text-center lg:text-left text-[20px]">Personalized support for your well being and mental health journey.</small>
 					</div>
 				</div>
 				<div class="better-life">
-					<img class="ceo__image about__images mx-auto" src="./static/images/about-us/about-001.png" alt="stage">
+					<img class="ceo__image about__images mx-auto rounded-2xl" src="./static/images/about-us/about-001.png" alt="stage">
 				</div>
 			</div>
 
-			<div class="ceo__row ceo__row--2">
+			<div class="ceo__row ceo__row--2 md:hidden border-b md:border-b-0">
 				<div class="grid lg:grid-cols-2 gap-20 items-center">
+					<div class="ceo__inner-row ceo__inner-row--1 grid">
+						<h3 class="text-[32px] font-bold">Our President & CEO</h3>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 					<div class="xoxo">
 						<div class="ceo-image">
 							<img src="./static/images/about-us/ceo.png" alt="" class="ceo__image mx-auto about__images rounded-2xl">
@@ -35,93 +39,85 @@
 					</div>
 					<div class="xoxo">
 						<div class="ceo__text-group">
-							<div class="ceo__inner-row ceo__inner-row--1 grid">
-								<h3 class="text-[56px] font-bold">Our President & CEO</h3>
-								<div class="tick__border-top mx-auto"></div>
-							</div>
 							<small class="ceo__small">
-								Our vision is to create a global community of empowered individuals who have the skills
-								and knowledge to proactively manage their mental health and lead fulfilling lives.
-								<br>
-								We envision a world where seeking psychological support is as natural as taking care of
-								one's physical health, and where our application serves as a trusted companion for
-								personal development and emotional well-being.
+								Sam Frerer stands at the crossroads of mental and physical well-being as both a fully qualified Psychologist and Personal  Trainer. Currently, she serves with dedication as an Army Psychologist (Officer) in the Australian Defence Force, a role that requires a deep understanding of human psychology under the challenging environment of military service. <br>Graduating with a Bachelor of Psychology (Honours) from the renowned Monash University in 2018, Sam's thirst for knowledge didn't stop there. She ventured into the demanding  landscape of Defence to complete her internship, where she not only honed her skills but also achieved General Registration. Over the years, Sam has had the privilege of working with hundreds of clients, reflecting a tapestry of human experiences. Prior to her role as a Psychologist, her stint as a Personal Trainer equipped her with insights into the symbiotic relationship between mental strength and physical health. <br>This unique blend of expertise makes Sam a beacon for those seeking a balanced, holistic approach to wellness. Beyond her professional commitments, Sam is an ardent believer in the power of resilience, both of the body and the mind, and she continues to empower individuals in their personal journeys toward optimal health.
 							</small>
 						</div>
 					</div>
 				</div>
 			</div>
+			<div class="ceo__row ceo__row--2 hidden md:block">
+				<div class="content py-2 px-10">
+					<img class="float-left pr-8 pb-8 ceo__image" src="./static/images/about-us/ceo.png" alt="">
+					<div class="ceo__inner-row ceo__inner-row--1 grid mb-8">
+						<h3 class="text-[56px] font-bold">Founder and CEO: Sam Frerer</h3>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
+					<small class="ceo__small">
+					Sam Frerer stands at the crossroads of mental and physical well-being as both a fully qualified Psychologist and Personal  Trainer. Currently, she serves with dedication as an Army Psychologist (Officer) in the Australian Defence Force, a role that requires a deep understanding of human psychology under the challenging environment of military service. <br>Graduating with a Bachelor of Psychology (Honours) from the renowned Monash University in 2018, Sam's thirst for knowledge didn't stop there. She ventured into the demanding  landscape of Defence to complete her internship, where she not only honed her skills but also achieved General Registration. Over the years, Sam has had the privilege of working with hundreds of clients, reflecting a tapestry of human experiences. Prior to her role as a Psychologist, her stint as a Personal Trainer equipped her with insights into the symbiotic relationship between mental strength and physical health. <br>This unique blend of expertise makes Sam a beacon for those seeking a balanced, holistic approach to wellness. Beyond her professional commitments, Sam is an ardent believer in the power of resilience, both of the body and the mind, and she continues to empower individuals in their personal journeys toward optimal health.
+				</div>
+			</div>
 		</section>
 
-		<section class="ourstory">
+		<section class="ourstory border-b md:border-b-0">
 			<div class="ourstory__row ourstory__row--1">
-				<h3 class="ceo__heading ceo__heading--3 text-[56px]">Our Story</h3>
+				<h3 class="text-[32px] font-bold">Our Story</h3>
 				<div class="py-20">
 					<img src="./static/images/about-us/Image 6.png" alt="our story is togetherness" class="mx-auto rounded-2xl">
 				</div>
-				
+
 			</div>
-			<div class="ourstory__row ourstory__row--3">
-				<p class="ceo__small">
-					Our story is one of passion, empathy, and a commitment to providing the resources and tools needed
-					to
-					empower
-					individuals like you on your path to mental wellness. <br> <br>
-					It all started with a simple yet profound realization: mental health is just as important as
-					physical
-					health.
-					In a world that often prioritizes the visible, we recognized the need to create a space where the
-					invisible
-					struggles could find a voice. Thus, PsychInsight was born. <br> <br>
-					At PsychInsight, we understand that the journey to mental wellness is unique for each individual.
-					That's why
-					we've created a platform that caters to your specific needs, whether you're seeking guidance,
-					support, or
-					simply looking to enhance your mental well-being. We are here to provide you with the tools and
-					resources
-					that
-					empower you to take control of your mental health journey. <br> <br>
-					As we continue to grow and evolve, our dedication to promoting mental wellness remains unwavering.
-					Our story
-					is your story – a story of resilience, hope, and the belief that with the right insights and tools,
-					you can
-					achieve the mental well-being you deserve.
+			<div class="ourstory__row ourstory__row--3 bg-left ">
+				<p class="ourstory__small">
+					Our story is one of passion, empathy, and a commitment to providing the resources and tools needed to empower individuals like you on your path to mental wellness. <br> <br> It all started with a simple yet profound realization: mental health is just as important as physical health. In a world that often prioritizes the visible, we recognized the need to create a space where the invisible struggles could find a voice. Thus, PsychInsight was born. <br> <br> At PsychInsight, we understand that the journey to mental wellness is unique for each individual. That's why we've created a platform that caters to your specific needs, whether you're seeking guidance, support, or simply looking to enhance your mental well-being. We are here to provide you with the tools and resources that empower you to take control of your mental health journey. <br> <br> As we continue to grow and evolve, our dedication to promoting mental wellness remains unwavering. Our story is your story – a story of resilience, hope, and the belief that with the right insights and tools, you can achieve the mental well-being you deserve.
 				</p>
 			</div>
 		</section>
 
-		<section class="mission">
+		<section class="mission ">
 			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2">
-				<div class="values__col md:order-2">
+				<div class="lg:hidden">
+					<h4 class="values__heading font-bold">Mission</h4>
+					<div class="tick__border-top mx-auto"></div>
+				</div>
+				<div class="values__col lg:order-2">
 					<img class="values__image" src="static/images/about-us/001.png" alt="people holding hands in a .png picture">
 				</div>
 				<div class="values__col values__col--text  md:order-1">
-					<h4 class="values__heading font-bold">Mission</h4>
-					<div class="tick__border-top mx-auto"></div>
+					<div class="hidden lg:block">
+						<h4 class="values__heading font-bold">Mission</h4>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 					<p class="values__text">
-					Our aim with Psych Insight is to have an unwavering commitment to pursue knowledge and personal growth consistently. We strive to stay curious, open-minded, and always seek opportunities to expand our knowledge and skills. This enables us to adapt, innovate, and thrive in an ever-changing world.
+						Our aim with Psych Insight is to have an unwavering commitment to pursue knowledge and personal growth consistently. We strive to stay curious, open-minded, and always seek opportunities to expand our knowledge and skills. This enables us to adapt, innovate, and thrive in an ever-changing world.
 					</p>
 				</div>
 			</div>
 			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2">
-				<div class="values__col md:order-1">
-					<img class="values__image" src="static/images/about-us/002.png" alt="two people holding hands in a .png picture">
-				</div>
-				<div class="values__col values__col--text md:order-2">
+				<div class="lg:hidden">
 					<h4 class="values__heading font-bold">Vision</h4>
-								<div class="tick__border-top mx-auto"></div>
+					<div class="tick__border-top mx-auto"></div>
+				</div>
+				<div class="values__col lg:order-1">
+					<img class="values__image" src="static/images/about-us/vision.png" alt="two people holding hands in a .png picture">
+				</div>
+				<div class="values__col values__col--text lg:order-2">
+					<div class="hidden lg:block">
+						<h4 class="values__heading font-bold">Vision</h4>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 					<p class="values__text">
-					In the nearest future, we envision Psych Insight is to have an unwavering commitment to pursue knowledge and personal growth consistently. We strive to stay curious, open-minded, and always seek opportunities to expand our knowledge and skills. This enables us to adapt, innovate, and thrive in an ever-changing world.
+						In the nearest future, we envision Psych Insight is to have an unwavering commitment to pursue knowledge and personal growth consistently. We strive to stay curious, open-minded, and always seek opportunities to expand our knowledge and skills. This enables us to adapt, innovate, and thrive in an ever-changing world.
 					</p>
 				</div>
 			</div>
 		</section>
 
-		<section class="values">
+		<section class="values ">
 			<div class="values__row values__row--heading grid">
-				<h3 class="ceo__heading ceo__heading--3 font-bold text-[56px]" style="text-align: center;">Our Values and Culture</h3>
+				<h3 class="ceo__heading ceo__heading--3 font-bold text-[56px] space-y-2" style="text-align: center;">Our Values and Culture</h3>
 			</div>
-			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2">
+			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2 hidden md:block">
 				<div class="values__col md:order-2">
 					<img class="values__image" src="static/images/about-us/001.png" alt="people holding hands in a .png picture">
 				</div>
@@ -134,36 +130,54 @@
 				</div>
 			</div>
 			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2">
+					<div class="md:block">
+						<h4 class="values__heading font-bold">Compassion</h4>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 				<div class="values__col md:order-1">
 					<img class="values__image" src="static/images/about-us/002.png" alt="two people holding hands in a .png picture">
 				</div>
 				<div class="values__col values__col--text md:order-2">
-					<h4 class="values__heading font-bold">Compassion</h4>
-					<div class="tick__border-top mx-auto"></div>
+					<div class="hidden lg:block">
+						<h4 class="values__heading font-bold">Compassion</h4>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 					<p class="values__text">We approach our users with empathy and compassion, understanding the
 						challenges they
 						may be facing and providing support without judgment</p>
 				</div>
 			</div>
 			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2">
+				<div class="md:hidden">
+					<h4 class="values__heading font-bold">Integrity</h4>
+					<div class="tick__border-top mx-auto"></div>
+				</div>
 				<div class="values__col  md:order-2">
 					<img class="values__image" src="static/images/about-us/003.png" alt="two people shaking hands in a .png picture">
 				</div>
 				<div class="values__col values__col--text md:order-1">
-					<h4 class="values__heading font-bold">Integrity</h4>
-					<div class="tick__border-top mx-auto"></div>
+					<div class="hidden lg:block">
+						<h4 class="values__heading font-bold">Integrity</h4>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 					<p class="values__text">We operate with transparency and integrity, ensuring that our application's
 						content
 						and features align with our mission and the best interests of our users.</p>
 				</div>
 			</div>
 			<div class="grid md:grid-cols-2 items-center gap-10 border-b md:border-b-0 pb-2">
+				<div class="md:hidden">
+					<h4 class="values__heading font-bold">Community</h4>
+					<div class="tick__border-top mx-auto"></div>
+				</div>
 				<div class="values__col md:order-1">
 					<img class="values__image" src="static/images/about-us/004.png" alt="two people shaking hands in a .png picture">
 				</div>
 				<div class="values__col values__col--text  md:order-2">
-					<h4 class="values__heading font-bold">Community</h4>
-					<div class="tick__border-top mx-auto"></div>
+					<div class="hidden lg:block">
+						<h4 class="values__heading font-bold">Community</h4>
+						<div class="tick__border-top mx-auto"></div>
+					</div>
 					<p class="values__text">We foster a sense of community among our users, providing a platform for
 						sharing
 						experiences, insights, and support in a safe and respectful environment.
