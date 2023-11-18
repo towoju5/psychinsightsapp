01ee1efa5c919f72112793f4bca54bca22b090ca
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-11-18 02:20:17 +0100

    hamburger size adjusted

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 				</div>
 				<button data-collapse-toggle="navbar-cta" type="button" class="lg:hidden" aria-expanded="false">
 					<span class="sr-only">Open main menu</span>
-					<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+					<svg class="w-10 h-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
 						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"></path>
 					</svg>
 				</button>
